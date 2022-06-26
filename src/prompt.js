6d7d1ac77435896c69b752611e06e99fb889f78b
username555-git <106167243+username555-git@users.noreply.github.com>
2022-06-26 16:26:44 +0800

    #lesson 5
    inclusion of API

--- src/prompt.js
+++ src/prompt.js
@@ -23,7 +23,8 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -50,7 +51,13 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "6bcbbb16338664d130a554dd9384b956";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showTemperature(response) {
@@ -70,6 +77,7 @@ function showTemperature(response) {
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -118,4 +126,3 @@ let cLink = document.querySelector("#cLink");
 cLink.addEventListener("click", showCtemperature);
 
 searchCity("Singapore");
-displayForecast();
